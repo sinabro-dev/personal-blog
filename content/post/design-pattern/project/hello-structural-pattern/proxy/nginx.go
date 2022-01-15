ec94104cd9816ba38404daf07040ed911c74d16a
Joon <tmdgh0221@gmail.com>
2022-01-15 10:45:27 +0900

    practice proxy design pattern

---
+++ content/post/design-pattern/project/hello-structural-pattern/proxy/nginx.go
@@ -0,0 +1,34 @@
+package proxy
+
+type nginx struct {
+	application       *application
+	maxAllowedRequest int
+	rateLimiter       map[string]int
+}
+
+func newNginxServer() *nginx {
+	return &nginx{
+		application:       newApplication(),
+		maxAllowedRequest: 2,
+		rateLimiter:       make(map[string]int),
+	}
+}
+
+func (n *nginx) handleRequest(url, method string) (int, string) {
+	allowed := n.checkRateLimiting(url)
+	if !allowed {
+		return 403, "Not Allowed"
+	}
+	return n.application.handleRequest(url, method)
+}
+
+func (n *nginx) checkRateLimiting(url string) bool {
+	if _, ok := n.rateLimiter[url]; ok {
+		n.rateLimiter[url] = 1
+	}
+	if n.rateLimiter[url] > n.maxAllowedRequest {
+		return false
+	}
+	n.rateLimiter[url] += 1
+	return true
+}
